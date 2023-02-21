a336d08f4591977f6d88856867e04e0e7b75be10
Hunter <hunter.barclay18@gmail.com>
2023-02-20 17:07:27 -0700

    switching to laptop

---
+++ engine/Assets/Scripts/UI/Dynamic/ScreenspaceMarkers/ScreenspaceMarkerContainer.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#nullable enable
+
+namespace Synthesis.UI.Dynamic {
+
+    public class ScreenspaceMarkerContainer : Content {
+        
+    }
+
+}
