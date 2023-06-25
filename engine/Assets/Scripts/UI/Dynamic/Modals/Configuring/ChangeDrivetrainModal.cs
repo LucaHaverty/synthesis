da09d91587f78c6c0eb5287c17621c3f29e60215
LucaHaverty <lucahaverty@gmail.com>
2023-06-25 16:32:02 -0500

    Added option to spawn a robot in change drivetrain modal if no robot is found

--- engine/Assets/Scripts/UI/Dynamic/Modals/Configuring/ChangeDrivetrainModal.cs
+++ engine/Assets/Scripts/UI/Dynamic/Modals/Configuring/ChangeDrivetrainModal.cs
@@ -2,35 +2,78 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Synthesis.PreferenceManager;
+using Synthesis.UI;
 using Synthesis.UI.Dynamic;
 using UnityEngine;
+
+// ReSharper disable ConditionIsAlwaysTrueOrFalse
+// ReSharper disable HeuristicUnreachableCode
 
 public class ChangeDrivetrainModal : ModalDynamic {
 
     public const float MODAL_WIDTH = 400f;
-    public const float MODAL_HEIGHT = 100f;
+
+    public static float MODAL_HEIGHT
+    {
+        get
+        {
+            if (RobotSimObject.GetCurrentlyPossessedRobot() != null)
+                return 100f;
+            else
+                return 70f;
+        }
+    }
 
     private RobotSimObject.DrivetrainType _selectedType;
+    
+    public Func<Button, Button> DisableButton = b =>
+        b.StepIntoImage(i => i.SetColor(ColorManager.SYNTHESIS_BLACK_ACCENT))
+            .StepIntoLabel(l => l.SetColor(ColorManager.SYNTHESIS_ORANGE_CONTRAST_TEXT))
+            .DisableEvents<Button>();
+        
+    public Func<UIComponent, UIComponent> VerticalLayout = (u) => {
+        var offset = (-u.Parent!.RectOfChildren(u).yMin) + 7.5f;
+        u.SetTopStretch<UIComponent>(anchoredY: offset);
+        return u;
+    };
 
     public ChangeDrivetrainModal(): base(new Vector2(MODAL_WIDTH, MODAL_HEIGHT)) { }
-    public override void Create() {
+    public override void Create()
+    {
+        bool robotExists = RobotSimObject.GetCurrentlyPossessedRobot() != null;
+        
         Title.SetText("Change Drivetrain");
 
-        if (RobotSimObject.GetCurrentlyPossessedRobot() == null)
-            return;
-        AcceptButton.AddOnClickedEvent(b =>
+        if (robotExists)
         {
-            RobotSimObject.GetCurrentlyPossessedRobot().ConfiguredDrivetrainType = _selectedType;
-            DynamicUIManager.CloseActiveModal();
-        });
+            Description.SetText("Select a drivetrain for this robot");
 
-        _selectedType = RobotSimObject.GetCurrentlyPossessedRobot().ConfiguredDrivetrainType;
+            AcceptButton.AddOnClickedEvent(b =>
+            {
+                RobotSimObject.GetCurrentlyPossessedRobot().ConfiguredDrivetrainType = _selectedType;
+                DynamicUIManager.CloseActiveModal();
+            });
 
-        MainContent.CreateLabeledDropdown().SetTopStretch<LabeledDropdown>().StepIntoLabel(l => l.SetText("Type"))
-            .StepIntoDropdown(d => d.SetOptions(RobotSimObject.DRIVETRAIN_TYPES.Select(x => x.Name).ToArray())
-                .AddOnValueChangedEvent(
-                    (d, i, o) => _selectedType = RobotSimObject.DRIVETRAIN_TYPES[i])
-                .SetValue(_selectedType.Value));
+            _selectedType = RobotSimObject.GetCurrentlyPossessedRobot().ConfiguredDrivetrainType;
+
+            MainContent.CreateLabeledDropdown().SetTopStretch<LabeledDropdown>().StepIntoLabel(l => l.SetText("Type"))
+                .StepIntoDropdown(d => d.SetOptions(RobotSimObject.DRIVETRAIN_TYPES.Select(x => x.Name).ToArray())
+                    .AddOnValueChangedEvent(
+                        (d, i, o) => _selectedType = RobotSimObject.DRIVETRAIN_TYPES[i])
+                    .SetValue(_selectedType.Value));
+        }
+        else
+        {
+            AcceptButton.ApplyTemplate(DisableButton);
+            Description.SetText("Spawn a robot to change it's drivetrain");
+            
+            var spacing = 15f;
+            var button = MainContent.CreateButton("Spawn Robot")
+                .ApplyTemplate<Button>(VerticalLayout)
+                .AddOnClickedEvent(b => DynamicUIManager.CreateModal<AddRobotModal>())
+                .StepIntoLabel(l => l.SetText("Spawn Robot"));
+        }
     }
     public override void Update() { }
     public override void Delete() { }
